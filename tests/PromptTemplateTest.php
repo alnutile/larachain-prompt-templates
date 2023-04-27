d7724951e318389b64ba78b6d1e0ab27306b9a25
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2023-04-26 23:26:02 -0400

    add another test

--- tests/PromptTemplateTest.php
+++ tests/PromptTemplateTest.php
@@ -24,3 +24,32 @@ EOD;
     assertEquals('I want you to act as a naming consultant for new companies.
 What is a good name for a company that makes colorful socks?', $dto->format());
 });
+
+it('can replace multiple items', function () {
+    $template = <<<'EOD'
+I want you to act as a naming consultant for new companies.
+What is a good name for a company that makes {product1}, {product2} and {product3}?
+EOD;
+
+    $input_variables = [
+        new PromptToken(
+            'product1',
+            'colorful socks'
+        ),
+        new PromptToken(
+            'product2',
+            'colorful hats'
+        ),
+        new PromptToken(
+            'product3',
+            'colorful headbands'
+        )
+    ];
+    $dto = new PromptTemplate(
+        $input_variables,
+        $template
+    );
+
+    assertEquals('I want you to act as a naming consultant for new companies.
+What is a good name for a company that makes colorful socks, colorful hats and colorful headbands?', $dto->format());
+});
